d0735a22785779a0a47e5aaef301a27b422ee6c1
baheem ferrell <beezhair@baheems-air.mynetworksettings.com>
2025-03-04 20:01:05 -0500

    Fix: Add formatDate export to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { formatDate, formatCurrency, formatDateRange } from "./utils/format";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Re-export format utilities
+export { formatDate, formatCurrency, formatDateRange };
